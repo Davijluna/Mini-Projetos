a2aee63c92b6b20999b08713e08ef7d32a3877c2
davi <davijluna@gmail.com>
2024-01-13 13:30:21 -0300

    feat: adicionando exercicio 1001 e 1010

---
+++ Exercicios-Breecrud/problems/1001.js
@@ -0,0 +1,6 @@
+// ** Extremamente Básico;
+export function problem (input) {
+  const [value1, value2] = input
+  const soma = parseInt(value1) + parseInt(value2);
+  console.log(`X = ${soma}`)
+}
--- Exercicios-Breecrud/problems/1010.js
+++ Exercicios-Breecrud/problems/1010.js
@@ -1,17 +1,7 @@
-export function problem (input) {
-  const [
-     codePartOne,
-     numberPartOne, 
-     valuePartOne, 
-     codePartTwo, 
-     numberPartTwo, 
-     valuePartTwo] = input
-
-     const Total = (valuePartOne * numberPartOne) + (valuePartTwo * numberPartTwo);
-
-
-
+export function problem (lines) {
+  const space = ' '
+  const [codePartOne, numberPartOne, valuePartOne] = lines[0].split(space).map(Number)
+  const [codePartTwo, numberPartTwo, valuePartTwo] = lines[1].split(space).map(Number)
+  const Total = numberPartOne * valuePartOne + numberPartTwo * valuePartTwo;
   console.log(`VALOR A PAGAR: R$ ${Total.toFixed(2)}`)
-
-
 }
