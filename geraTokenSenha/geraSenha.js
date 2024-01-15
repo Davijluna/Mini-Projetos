c67b238a48370df79e4d65a1cc6e00550b9d2fdd
davi <davijluna@gmail.com>
2024-01-15 16:25:34 -0300

    feat: adicionando gerador de senha

---
+++ geraTokenSenha/geraSenha.js
@@ -0,0 +1,13 @@
+//**Gerador de Senha */
+
+function geraSenha(comprimento) {
+  let caracteres = 'ABCDEFGHIJLMNOPQRSTUVXZWYabcdefghijlmnopqrstuvxzwy1234567890_'
+  let senha =''
+  for(let i = 0; i < comprimento; i+= 1) {
+    let random = Math.floor(Math.random() * caracteres.length)
+    senha += caracteres[random]
+  }
+  return senha
+}
+
+console.log(geraSenha(37));
