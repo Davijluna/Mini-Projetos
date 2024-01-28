91c845a09d31c2a2b4dae60f731bd982d4fb5c17
davi <davijluna@gmail.com>
2024-01-28 12:56:37 -0300

    feat: Mensagens de erro no formulário

--- Controle-de-Gastos/index.js
+++ Controle-de-Gastos/index.js
@@ -1,15 +1,20 @@
-function validateFirlds() {
-  const emailValid = isEmailValid();
-  document.getElementById('recover-password-button').disabled = !emailValid;
+const emailValid = document.querySelector('.Input-email')
+console.log(emailValid.innerHTML)
 
-  const passwordValid = isPasswordValid();
-  document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+function onChangPassword() {
+  toggleButtonsDisable();
+  togglePasswordErros();
+}
+
+function onChangeEmail() {
+  toggleButtonsDisable();
+  toggleEmailErrors();
+  //  ! togglePasswordErros() "foi removido "
 }
 
 function isEmailValid() {
   const email = document.getElementById("email").value;
   if (!email) {
-    console.log('passei aqui !!!!')
     return false;
   }
   return validateEmail(email);
@@ -25,4 +30,37 @@ function isPasswordValid() {
 
 function validateEmail(email) {
   return /\S+@\S+\.\S+/.test(email);
+}
+
+
+function toggleEmailErrors() {
+   const email = document.getElementById('email').value;
+   if (!email) {
+    document.getElementById('email-required-error').style.display = "block";
+   } else {
+    document.getElementById('email-required-error').style.display = "none";
+   }
+
+   if (validateEmail(email)) {
+    document.getElementById('email-invalid-error').style.display = "none";
+   } else {
+    document.getElementById('email-invalid-error').style.display = "block"
+   }
+}
+
+function togglePasswordErros() {
+  const password = document.getElementById('password').value;
+  if (!password) {
+    document.getElementById('password-required-error').style.display = "block";
+  } else {
+    document.getElementById('password-required-error').style.display = "none";
+  }
+ }
+
+function toggleButtonsDisable() {
+  const emailValid = isEmailValid();
+  document.getElementById('recover-password-button').disabled = !emailValid;
+
+  const passwordValid = isPasswordValid();
+  document.getElementById('login-button').disabled = !emailValid || !passwordValid;
 }
