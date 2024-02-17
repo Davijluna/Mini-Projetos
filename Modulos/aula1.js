568206358bc0b43050543cafe798c5b22a7afa58
davi <davijluna@gmail.com>
2024-02-17 16:24:51 -0300

    feat: criando arquivos para exericio de modulo

--- Modulos/aula1.js
+++ Modulos/aula1.js
@@ -1,4 +1,15 @@
-import Cursos from './conteudo.js'
-Cursos.addCurso("PhotoShop")
-Cursos.apagaCurso()
-console.log(Cursos.getTodosCursos())
+import { contatos } from "./banco Contatos";
+
+let contato = {
+  getTodosContatos: function() {
+    return contatos
+  }
+}
+
+export default contato
+
+
+// import Cursos from './conteudo.js'
+// Cursos.addCurso("PhotoShop")
+// Cursos.apagaCurso()
+// console.log(Cursos.getTodosCursos())
