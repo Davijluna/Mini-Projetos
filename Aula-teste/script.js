d8a6b88deb3b719db11f6d89a0df1f33df76ad40
davi <davijluna@gmail.com>
2024-02-24 23:15:18 -0300

    feat: revisão que fiz de casos de exeção na Trybe

---
+++ Aula-teste/script.js
@@ -0,0 +1,37 @@
+// Primeira parte
+const promo = [
+  {product: 'bola de beach tennis', price: 29.99},
+  {product: 'mochila de trilha 20 litros', price: 120.99},
+  {product: 'capacete de ciclista', price: 180.59},
+  {product: 'toalha compacta de secagem', price: 39.99},
+  {product: 'kit 5 camisetas Dry Fit', price: 99.99},
+  {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
+  {product: 'bola de ginástica e pilates', price: 149.59},
+  {product: 'óculos de natação', price: 18.99},
+  {product: 'capa protetora de prancha de surf', price: 89.59},
+  {product: 'bola de basquete', price: 39.99},
+];
+
+const button = document.querySelector('#send-button');
+
+button.addEventListener('click', () => {
+  const name = document.querySelector('#name-id').value;
+  const number = document.querySelector('#number-id').value;
+
+  showPromo(name, number);
+});
+
+const showPromo = (name, number) => {
+  const firstText = document.querySelector('#text-initial');
+  const secondText = document.querySelector('#text-final');
+  const productObject = checkNumber(parseInt(number));
+
+  firstText.innerHTML = `Boas-vindas, ${name}!`;
+  secondText.innerHTML = `A promoção do dia é: 
+    ${productObject.product} no valor de R$ ${productObject.price}`;
+}
+
+// Segunda parte
+const checkNumber = (number) => {
+  return promo[number - 1];
+}
