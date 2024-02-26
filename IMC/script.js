ad2f33104ca819e137fc7784f3b8793028225376
davi <davijluna@gmail.com>
2024-02-26 12:51:50 -0300

    feat: adicionando as validações de peso do imc

--- IMC/script.js
+++ IMC/script.js
@@ -1,23 +1,38 @@
 
 const inputAltura = document.querySelector('#altura');
 const inputPeso = document.querySelector('#peso');
-
+const valorIMC = document.querySelector('.returnoIMC')
 
 const calculo = () => {
-  
   const peso = parseFloat(inputPeso.value);
   const altura = parseFloat(inputAltura.value);
 
   const valeuPeso = peso;
   const valeAltura = altura;
-  
-  console.log(typeof altura, 'altura')
 
-  if(typeof valeAltura !== 'number' || typeof valeuPeso !== "number") {
-   console.log("Digite um número para o calculo!!!!")
-  } else {
-    const IMC =  valeuPeso / (valeAltura * valeAltura)
-    console.log(IMC)
+  if(isNaN(valeAltura) || isNaN(valeuPeso)) {
+   alert("Digite um número para o calculo!!!!")
   }
-  
+    const IMC =  valeuPeso / (valeAltura * valeAltura)
+    console.log(filtraIMC(IMC.toFixed(2)))
+    valorIMC.innerText = filtraIMC(IMC.toFixed(2))
+
+}
+
+const filtraIMC = (imc) => {
+  if(imc < 18.5) {
+    return 'Magreza	0'
+  }
+  if(imc >= 18.5 && imc <= 24.9) {
+    return 'Normal	0'
+  }
+  if(imc >= 25.0 && imc <= 29.9) {
+    return 'Sobrepeso	I'
+  }
+  if(imc >= 30.0 && imc <= 39.9) {
+    return 'Obesidade	II'
+  }
+  if(imc >= 40.0) {
+    return 'Obesidade Grave	III'
+  }
 }
