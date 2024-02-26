9d09e50dd0996cb57736b9b42879baad958e1fd8
davi <davijluna@gmail.com>
2024-02-26 14:34:04 -0300

    refactor: reratorando função filtraIMC e alterando para ao quadrado altura e adicionando validação de altura com regex

--- IMC/script.js
+++ IMC/script.js
@@ -4,35 +4,30 @@ const inputPeso = document.querySelector('#peso');
 const valorIMC = document.querySelector('.returnoIMC')
 
 const calculo = () => {
+  /\d{1}\.?\d{2}/
   const peso = parseFloat(inputPeso.value);
   const altura = parseFloat(inputAltura.value);
 
   const valeuPeso = peso;
   const valeAltura = altura;
 
+  if(/\d{1}\.?\d{2}/.test(valeAltura)) {
+    alert("Altura deve ter ponto decimal")
+    return valorIMC.innerText = 'Altura não têm ponto'
+  }
   if(isNaN(valeAltura) || isNaN(valeuPeso)) {
    alert("Digite um número para o calculo!!!!")
   }
-    const IMC =  valeuPeso / (valeAltura * valeAltura)
+    const IMC =  valeuPeso / (valeAltura **2)
     console.log(filtraIMC(IMC.toFixed(2)))
     valorIMC.innerText = filtraIMC(IMC.toFixed(2))
 
 }
 
 const filtraIMC = (imc) => {
-  if(imc < 18.5) {
-    return 'Magreza	0'
-  }
-  if(imc >= 18.5 && imc <= 24.9) {
-    return 'Normal	0'
-  }
-  if(imc >= 25.0 && imc <= 29.9) {
-    return 'Sobrepeso	I'
-  }
-  if(imc >= 30.0 && imc <= 39.9) {
-    return 'Obesidade	II'
-  }
-  if(imc >= 40.0) {
-    return 'Obesidade Grave	III'
-  }
+  if(imc < 18.5) return 'Magreza	0';
+  if(imc >= 18.5 && imc <= 24.9) return 'Normal	0';
+  if(imc >= 25.0 && imc <= 29.9) return 'Sobrepeso	I';
+  if(imc >= 30.0 && imc <= 39.9) return 'Obesidade	II';
+  if(imc >= 40.0) return 'Obesidade Grave	III';
 }
