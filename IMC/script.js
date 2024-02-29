ed5068e0007f78d48cc4ee21d7a81dc7aa2be6a2
davi <davijluna@gmail.com>
2024-02-28 23:14:55 -0300

    refactor: alteração simples nas codições

--- IMC/script.js
+++ IMC/script.js
@@ -8,6 +8,7 @@ const calculo = () => {
   const altura = parseFloat(inputAltura.value);
 
   if(inputPeso.value === '' || inputAltura.value === '') {
+    valorIMC.className = 'colorOrange';
     alert('Altura ou peso está vazio.')
     return valorIMC.innerText = 'Campos estão vazios'
   }
@@ -18,6 +19,7 @@ const calculo = () => {
   }
 
   if(!/^-?\d+\.\d+$/.test(inputAltura.value)) {
+    valorIMC.className = 'colorOrange';
     alert("Altura deve ter ponto decimal")
     return valorIMC.innerText = 'Altura não têm ponto'
   }
@@ -29,9 +31,24 @@ const calculo = () => {
 }
 
 const filtraIMC = (imc) => {
-  if(imc < 18.5) return 'Magreza	0';
-  if(imc >= 18.5 && imc <= 24.9) return 'Normal	0';
-  if(imc >= 25.0 && imc <= 29.9) return 'Sobrepeso	I';
-  if(imc >= 30.0 && imc <= 39.9) return 'Obesidade	II';
-  if(imc >= 40.0) return 'Obesidade Grave	III';
+  if(imc < 18.5) {
+    valorIMC.className = 'colorRed';
+    return 'Magreza	0';
+  }
+  if(imc >= 18.5 && imc <= 24.9) {
+    valorIMC.className = 'colorClear';
+    return 'Normal	0';
+  }
+  if(imc >= 25.0 && imc <= 29.9) {
+    valorIMC.className = 'colorOrange';
+    return 'Sobrepeso	I';
+  }
+  if(imc >= 30.0 && imc <= 39.9) {
+    valorIMC.className = 'colorRed';
+    return 'Obesidade	II';
+  }
+  if(imc >= 40.0) {
+    valorIMC.className = 'colorRed';
+    return 'Obesidade Grave	III';
+  }
 }
