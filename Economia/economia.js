58d2c054a5e92ae1394b11bac2f6df444175c7db
davi <davijluna@gmail.com>
2024-03-08 20:02:23 -0300

    feat: adicionando buttons a página

--- Economia/economia.js
+++ Economia/economia.js
@@ -4,7 +4,19 @@ let getParagrafos = document.querySelector('#paragrafos')
 const actionValor = () => {
   const creatTag = document.createElement('p');
   creatTag.innerText += getValores.value
+  getValores.value = ''
+  
+  // criando button
+  
+  const buttonRemove = document.createElement('button')
+  buttonRemove.innerText = 'Excluir';
   getParagrafos.appendChild(creatTag)
+  getParagrafos.appendChild(buttonRemove).onclick(remove())
   return getParagrafos
-
 }
+
+
+const remove = () => {
+  console.log('Hello World !!')
+}
+
