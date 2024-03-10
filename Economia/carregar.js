4c80de9f135d9f4dbe98862edaf746716fd6b40e
davi <davijluna@gmail.com>
2024-03-10 00:56:41 -0300

    feat: criando read de planilha

--- Economia/carregar.js
+++ Economia/carregar.js
@@ -1,10 +1,62 @@
-const fs = require('fs')
-const caminho = 'arquivo.txt'
+const fs = require('fs');
+const reader = require('xlsx')
+const filePath = './fornecedores.xlsx'
+const file = reader.readFile('./fornecedores.xlsx');
 
-fs.readFile(caminho, 'utf8', (err, data) => {
-  if(err) {
-    console.log('Ouve um erro no arquivo', err)
-  }
-    console.log('conteudo do arquivo >>>>>', data)
-})
+let data = []
 
+const sheets = file.SheetNames
+
+console.log(sheets)
+
+for(let index = 0; index < sheets.length; index += 1) {
+  const temp = reader.utils.sheet_to_json(
+    file.Sheets[file.SheetNames[index]])
+    temp.forEach((res) => {
+      data.push(res)
+    })
+}
+
+console.log(data)
+
+// 'Planilha1'
+
+// fs.readFile(filePath, 'utf8', (err, data) => {
+//   if(err) {
+//     console.log('Ouve um erro ao carregar o arquivo', err)
+//   }
+//     console.log('Conteúdo do arquivo:', data)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs')
+// const caminho = 'arquivo.txt'
+
+// fs.readFile(caminho, 'utf8', (err, data) => {
+//   if(err) {
+//     console.log('Ouve um erro no arquivo', err)
+//   }
+//     console.log('conteudo do arquivo >>>>>', data)
+// })
