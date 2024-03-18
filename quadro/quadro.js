987dd52fd0019148f0eb3475decffe27bcb57616
davi <davijluna@gmail.com>
2024-03-18 11:16:20 -0300

    feat: exercicio de site de teste

--- quadro/quadro.js
+++ quadro/quadro.js
@@ -1,7 +1,25 @@
 let number = 5;
+let sust = '#'
+let test = ''
 
-function post(value) {
-  for(let index = 0; index < value; index += 1) {
-    console.log('Hello World')
-  }
-}
+const myValue = 57
+const result = (myValue % 5) - 1
+const value = result + myValue
+
+
+// const result2 = 67 % 5 
+// const result3 = 38 % 5
+console.log(value, result)
+// console.log(result2)
+// console.log(result3)
+
+
+// for(let index = 0; index < number; index += 1) {
+//   test += sust
+//   for(let index2 = 0; index2 < number; index2 += 1) {
+//     test += `${test} \n`
+//   }
+// }
+
+
+// console.log(test)
