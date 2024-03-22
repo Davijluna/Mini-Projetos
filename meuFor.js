dc83cdb085453af647fcc902918140babc2187a6
davi <davijluna@gmail.com>
2024-03-22 13:40:31 -0300

    adicionando alterações antigas

---
+++ meuFor.js
@@ -0,0 +1,8 @@
+const array = ["Davi", "Ana", "Maria", "Paulo", "Felipe"];
+let recebe = [];
+
+for(let i = 0; i < array.length; i += 1) {
+  recebe.push(array[i])
+  console.log(recebe)
+  console.log(array[i]);
+}
