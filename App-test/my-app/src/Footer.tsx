0509e8149ea4d12b51b7789e0d8648d28189089b
davi <davijluna@gmail.com>
2024-04-29 14:04:34 -0300

    feat: adiconando cabeçalho e rodapé a página

---
+++ App-test/my-app/src/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className='stringFooter'>
+      MKS sistemas © Todos os direitos reservados
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ App-test/my-app/src/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+const Header = () => {
+  return (
+    <header className='headerTop'>
+      <p><h1>MKS</h1> Sistemas</p>
+     
+    </header>
+  )
+}
+
+export default Header
