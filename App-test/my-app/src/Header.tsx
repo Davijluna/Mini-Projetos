72faf5da307fa02546443af4d55d1f334a9ba527
davi <davijluna@gmail.com>
2024-05-02 14:38:53 -0300

    style: Adicionando styled-component ao projeto

--- App-test/my-app/src/Header.tsx
+++ App-test/my-app/src/Header.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import './index.css'
 import Carrinho from './img/Captura de tela de 2024-05-01 23-33-31.png'
 
+import styled from 'styled-components'
+
+
+const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: #BF4F74;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 const Header = () => {
   return (
     <header className='headerTop'>
@@ -9,7 +23,14 @@ const Header = () => {
       <p className='title'><h1>MKS</h1> <h2>Sistemas</h2></p>
 
     <div>
+      <Wrapper>
+        <Title>
+          Hello world!
+        </Title>
+      </Wrapper>
       <button className='botao' type='button'><img src={Carrinho} alt='Carrinho de compras'/> 1</button>
+
+
       </div>
     </div>
 
