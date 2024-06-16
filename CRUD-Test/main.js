70bdc618ee957b826d9e51a63cb37805b9727773
davi <davijluna@gmail.com>
2024-06-16 00:10:29 -0300

    feat: adicionando arquivos para suporte

--- CRUD-Test/main.js
+++ CRUD-Test/main.js
@@ -6,6 +6,30 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+  nome: "Paulo",
+  email: "paulo@gmail.com",
+  celular: "1112345678",
+  cidade: "São Paulo"
+}
+
+  // CRUD - create read update delete
+
+  // localStorage.setItem("db_client", JSON.stringify([]))
+
+  const createClient = (client) => {
+    const db_client =  JSON.parse(localStorage.getItem("db_client"))
+
+    if (!db_client) {
+      db_client = [];
+    }
+    
+    console.log(db_client)
+    db_client.push(client)
+    localStorage.setItem("db_client",JSON.stringify(db_client))
+  }
+//  createClient(tempClient)
+// Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
