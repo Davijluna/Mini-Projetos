ae49faece6b3e67d1eb2f9ba0bdd3ba404c681d6
davi <davijluna@gmail.com>
2024-06-17 13:02:40 -0300

    feat: deletando cliente

--- CRUD-Test/main.js
+++ CRUD-Test/main.js
@@ -16,6 +16,12 @@ const tempClient = {
 const getLocaStorage = () => JSON.parse(localStorage.getItem('db_client'))??[];
 const setLocalStorage = (dbClient) =>  localStorage.setItem('db_client', JSON.stringify(dbClient));
 
+const deleteClient = (index) => {
+  const dbClient = readClient()
+  dbClient.splice(index, 1)
+  setLocalStorage(dbClient)
+}
+
 const updateClient = (index, client) => {
   const dbClient = readClient()
   dbClient[index] = client
