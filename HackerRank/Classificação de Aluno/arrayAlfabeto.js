fb6eb622cbaf3d582453add3acb392768a3d3c97
davi <davijluna@gmail.com>
2024-07-29 23:12:07 -0300

    feat: função letras do alfabetos

---
+++ HackerRank/Classificação de Aluno/arrayAlfabeto.js
@@ -0,0 +1,18 @@
+let letrasArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N'];
+
+const functionLetras = (num) => {
+    let valueArray = []
+
+    for (let index = 0; index < num; index += 1) {
+        if (valueArray[index] == undefined) {
+            valueArray[index] = letrasArray[index]
+        } else {
+
+            valueArray[index] += letrasArray[index];
+        }
+    }
+    return valueArray
+
+};
+
+console.log(functionLetras(2));
