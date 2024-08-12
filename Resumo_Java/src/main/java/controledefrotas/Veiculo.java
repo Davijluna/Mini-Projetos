abec0aded609fb989f1ed9183e7a1fac6ccdb2b3
davi <davijluna@gmail.com>
2024-08-11 23:45:23 -0300

    Comentário

--- Resumo_Java/src/main/java/controledefrotas/Veiculo.java
+++ Resumo_Java/src/main/java/controledefrotas/Veiculo.java
@@ -2,4 +2,13 @@ package controledefrotas;
 
 public class Veiculo {
 
+  String placa;
+  int fabricacao;
+
+  String combustivel;
+
+//  Parei no minuto 13:00 da aula
+  public Veiculo() {
+    System.out.println("retorno do construtor ...");
+  }
 }
