59db4031e06400dd7c3f13917202ebb70a400331
davi <davijluna@gmail.com>
2024-08-11 23:39:12 -0300

    estanciando uma classe somente com construtor imbutido

---
+++ Resumo_Java/src/main/java/controledefrotas/Principal.java
@@ -0,0 +1,7 @@
+package controledefrotas;
+
+public class Principal {
+  public static void main(String[] args) {
+    Veiculo veiculo = new Veiculo();
+  }
+}
---
+++ Resumo_Java/src/main/java/controledefrotas/Veiculo.java
@@ -0,0 +1,5 @@
+package controledefrotas;
+
+public class Veiculo {
+
+}
