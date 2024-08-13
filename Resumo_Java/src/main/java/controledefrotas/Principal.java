ef20b28a44d587adbd7010edda37047e884c5226
davi <davijluna@gmail.com>
2024-08-12 22:13:59 -0300

    comentário

--- Resumo_Java/src/main/java/controledefrotas/Principal.java
+++ Resumo_Java/src/main/java/controledefrotas/Principal.java
@@ -3,5 +3,7 @@ package controledefrotas;
 public class Principal {
   public static void main(String[] args) {
     Veiculo veiculo = new Veiculo("XYZW", 1999);
+
+//    aula foi parada no intervalo
   }
 }
