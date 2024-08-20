105caf7b988403bfdb5495e04e0ed2f1b86e3c23
davi <davijluna@gmail.com>
2024-08-19 23:28:15 -0300

    feat: trinângulo retângulo

--- Triangulo_retangulo/Triangulo_retangulo.js
+++ Triangulo_retangulo/Triangulo_retangulo.js
@@ -4,3 +4,10 @@ for (let i = 0; i < 4; i +=1) {
     console.log(asteristico)
     asteristico += "*"
 }
+
+let asteristico = "*"
+
+for (let i = 0; i < 4; i +=1) {
+    console.log(asteristico)
+    asteristico += "*"
+}
