4e317b16f2415849494b5353e7237bb0b06238cd
davi <davijluna@gmail.com>
2024-11-15 09:48:53 -0300

    feat: Identificador de raças

--- IA_python/Criando_funcionalidades_Gemini/funcionalidade_com_genai.py
+++ IA_python/Criando_funcionalidades_Gemini/funcionalidade_com_genai.py
@@ -5,11 +5,20 @@ GOOGLE_API_KEY = os.environ["GEMINI_API"]
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-with open("curriculo.txt", "r") as file:
-    curriculum = file.read()
+# students_spreadsheet = genai.upload_file(path="identificador_estudantes.csv", display_name="Notas do ENEM")
 
-response = model.generate_content(
-    f"Pode, por favor, aprimorar o meu currículo para deixar ele mais assertivo e melhor: {curriculum}"
-    )
+# response = model.generate_content([students_spreadsheet, "Pode gerar um relatório de dois os três paragrafos baseado nesse dados?"])
+# print(response.text)
+
+
+# IDENTIFICADOR DE RAÇAS
+
+dog_image = genai.upload_file(
+    path="cachorro_golden_retriever.png"
+)
+
+response = model.generate_content([dog_image, "Pode identificar a raça do cachorro da foto e me dar duas ou três frases de informações a respeito dele? "
+   "De preferência, alguma curiosidade interessante em português, citando a fonte da informação e sempre de um jeito leve e interessante."
+])
 
 print(response.text)
