c391edd4d8b95fda20be1f6017bef7ff211e1db0
davi <davijluna@gmail.com>
2024-11-16 23:50:52 -0300

    feat:atualizando

--- IA_python/Criando_funcionalidades_Gemini/Criando_um_Chatbot/fucionalidade_chatbot.py
+++ IA_python/Criando_funcionalidades_Gemini/Criando_um_Chatbot/fucionalidade_chatbot.py
@@ -1,7 +1,10 @@
 import google.generativeai as genai
 import os
+import gradio as gr
+
 # Configure a chave de API
-genai.configure(api_key=os.environ["GEMINI_API"])
+GOOGLE_API_KEY = os.environ["GEMINI_API"]
+genai.configure(api_key=GOOGLE_API_KEY)
 # Escolha o modelo a ser usado
 model = genai.GenerativeModel("gemini-1.5-flash")
 
@@ -9,7 +12,13 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 chat = model.start_chat()
 
 # Envie uma mensagem para o chat
-response = chat.send_message("Olá")
+response = chat.send_message("Você é um consultor de projetos e amigo para quem quer aprender.")
 
+def gradio_wrapper(message, _history):
+    response = chat.send_message(message)
+    return response.text
 # Exiba a resposta
 print(response.text)
+
+chatInterface = gr.ChatInterface(gradio_wrapper)
+chatInterface.launch()
