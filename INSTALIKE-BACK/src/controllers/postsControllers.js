1749b23d3d3f5740b0744499bfeb39120149ba3e
davi <davijluna@gmail.com>
2024-12-24 17:36:37 -0300

    modificações

--- INSTALIKE-BACK/src/controllers/postsControllers.js
+++ INSTALIKE-BACK/src/controllers/postsControllers.js
@@ -1,4 +1,4 @@
-import getTodosPosts from "../models/postsModel.js";
+import {getTodosPosts, criarPost} from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
     // Chama a função para buscar os posts no banco de dados
@@ -6,3 +6,28 @@ export async function listarPosts(req, res) {
     // Envia os posts como resposta em formato JSON com status 200 (sucesso)
     res.status(200).json(posts);
 }
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost)
+        res.status(200).json(postCriado)
+    } catch (erro) {
+        console.error(erro.message)
+        res.status(500).json({"Erro": "Falha na requisição"})
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = req.body;
+
+    try {
+        const postCriado = await criarPost(novoPost)
+        res.status(200).json(postCriado)
+    } catch (erro) {
+        console.error(erro.message)
+        res.status(500).json({"Erro": "Falha na requisição"})
+    }
+}
+
--- INSTALIKE-BACK/src/routes/postsRoutes.js
+++ INSTALIKE-BACK/src/routes/postsRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsControllers.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsControllers.js";
+
+const upload = multer({dest: "./upload"})
 
 const routes = (app) => {
     // Habilita o parsing de JSON no corpo das requisições
     app.use(express.json());
     // Rota GET para obter todos os posts
     app.get("/posts", listarPosts);
+    // Rota para criar um post
+    app.post("/posts", postarNovoPost)
+
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
 export default routes;
