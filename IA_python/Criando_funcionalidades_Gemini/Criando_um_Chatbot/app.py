4306bbfad257486b3a9b8d43b9a58757208459e3
davi <davijluna@gmail.com>
2024-12-30 22:17:46 -0300

    feat: adicionando arquivos de teste para formulário com IA Generati

---
+++ IA_python/Criando_funcionalidades_Gemini/Criando_um_Chatbot/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route('/form')
+def form():
+    return render_template('form.html')
+@app.route('/feedback', methods=['POST'])
+def feedback():
+    feedback_text = request.form['feedback']
+    return f"Obrigado pelo seu feedback: {feedback_text}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
