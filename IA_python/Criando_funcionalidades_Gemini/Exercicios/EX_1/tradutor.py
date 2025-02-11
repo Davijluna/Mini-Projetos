61e9de432793a9b1c457382eaacf2eed0bd4a71e
davi <davijluna@gmail.com>
2025-02-10 23:47:02 -0300

    exercicio tradutor

--- IA_python/Criando_funcionalidades_Gemini/Exercicios/EX_1/tradutor.py
+++ IA_python/Criando_funcionalidades_Gemini/Exercicios/EX_1/tradutor.py
@@ -1,12 +1,30 @@
 # Importando as bibliotecas
-import google.gererativeai as genai
+import google.generativeai as genai
 import os
+import os.path
 
+path = './documento_ingles.txt'
+
+check_file = os.path.isfile(path)
+
+# os.environ["GRPC_VERBOSITY"] = "ERROR"
+# os.environ["GLOG_minloglevel"] = "2"
 # configurando a chave de API
 # GOOGLE_API_KEY = os.environ.get("GEMINI_API")
-genai.configure(api_key=os.environ["GEMIMI_API"]
+genai.configure(api_key=os.environ["GEMINI_API"])
+# genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
-# lendo o arquivo em português
-
-with open("documento_portugues.txt", "r") as file:
+with open("documento_portugues.txt", "r", encoding="utf-8") as file:
     arquivo = file.read()
+    prompt = f"Por favor, traduza o seguinte texto para o inglês:\n{arquivo}"
+    response = model.generate_content(prompt)
+
+print(check_file)
+
+if check_file == True:
+    print("Documento já existe no diretório.")
+else:
+    with open("documento_ingles.txt", "w", encoding="utf-8") as file:
+        file.write(response.text)
+        print("Tradução concluida e salva em 'documento_ingles.txt' .")
