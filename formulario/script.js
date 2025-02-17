bc9a88da8976983b4fb42310e4e2ba1158a92ad8
davi <davijluna@gmail.com>
2025-02-16 23:12:39 -0300

    feat:teste de formulário

--- formulario/script.js
+++ formulario/script.js
@@ -1,12 +1,26 @@
+let valor = {}
+
+
 document.querySelector("form").addEventListener("submit", function(event){
     event.preventDefault()
 
     let inputNome = document.getElementById("nome").value;
     let inputEmail = document.getElementById("email").value;
     let inputIdade = document.getElementById("idade").value;
-    
+
+    let info;
+
+    if (info === '')
+
+    info += inputNome
+    info += inputEmail
+    info += inputIdade
+
+    valor = info
     
     console.log(inputNome)
     console.log(inputEmail)
     console.log(inputIdade)
 })
+
+console.log(valor)
