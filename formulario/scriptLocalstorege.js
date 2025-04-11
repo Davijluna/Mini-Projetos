f183bf40e846fbc2d4647390b04527bddc5c5250
davi <davijluna@gmail.com>
2025-04-10 22:23:12 -0300

    feat: adicionando css para formulário de apredizagem

--- formulario/scriptLocalstorege.js
+++ formulario/scriptLocalstorege.js
@@ -1,15 +1,22 @@
 
 function salvar() {
     localStorage.info = document.getElementById("name").value
-
+    alert("Informações salvas !!!!")
+    location.reload()
 }
 
 function carregar() {
-    document.getElementById("name").value = localStorage.info
+    if (localStorage.info) document.getElementById("name").value = localStorage.info
+    if (!localStorage.info) alert("Não a mensagens a carregar ...")
+    
 }
 
 function remover() {
-    localStorage.inf = localStorage.removeItem
+    if (!localStorage.info) {
+        alert("mensagem já foi apagada!!!")
+    }
+    localStorage.clear()
+    location.reload()
 }
 
 console.log(nome)
