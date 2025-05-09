46523eaacc7bd6c85c202c21dab1d2a1bdeda745
davi <davijluna@gmail.com>
2025-05-08 23:15:31 -0300

    feat: criando o resumo com IA

--- IA_python/Criando_funcionalidades_Gemini/Exercicios/EX_2/resumo.py
+++ IA_python/Criando_funcionalidades_Gemini/Exercicios/EX_2/resumo.py
@@ -2,8 +2,7 @@
 
 import google.generativeai as genai
 import os
-import os.path
-from PyPDF2 import PdfReader
+import PyPDF2
 
 
 path = './paraResumir.txt'
@@ -15,19 +14,18 @@ genai.configure(api_key=os.environ["GEMINI_API"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-with open("The PageRank Citation Ranking- Bringing Order to the Web.pdf", "w", encoding="utf-8") as file:
-    reader = PdfReader(file)
+with open("The PageRank Citation Ranking- Bringing Order to the Web.pdf", "rb") as file:
+    reader = PyPDF2.PdfReader(file)
     texto = ""
 
     for page in reader.pages:
-        text += page.extract_text(path)
+        texto += page.extract_text()
 
-    prompt = f"Por favor, faça um resumo consiso de seguinte artigo cientifico:\n{text}"
+    prompt = f"Por favor, faça um resumo consiso de seguinte artigo cientifico:\n{texto}"
 
     response = model.generate_content(prompt)
 
 # teste = PyPDF2.PdfReader(path)
 
-
 print("Resumo do artigo cientifico:")
-print(response.txt)
+print(response.text)
